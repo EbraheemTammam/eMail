d988d8c19c1ca235060260a12f013da85a760363
Ebraheem <ebraheem20012020@gmail.com>
2024-11-02 06:38:13 +0200

    include thread and define sizes

--- SMTPServer.Core/Source/PCH.h
+++ SMTPServer.Core/Source/PCH.h
@@ -3,9 +3,15 @@
 #include <iostream>
 #include <fstream>
 #include <mutex>
+#include <thread>
 #include <memory>
 #include <string>
 #include <string_view>
 #include <chrono>
 #include <ctime>
 #include <format>
+#include <asio.hpp>
+
+
+#define BUFFER_SIZE (1024 >> 4)
+#define Q_SIZE 1024
