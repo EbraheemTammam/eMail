84b05e05e11daca9d1ac5b82eebbb6600e72ed5f
Ebraheem <ebraheem20012020@gmail.com>
2024-11-02 06:36:40 +0200

    extend logger and move it to utilities

---
+++ SMTPServer.Core/Source/Utilities/Logger.cpp
@@ -0,0 +1,102 @@
+#include "Logger.h"
+
+namespace SMTPServer::Core::Utilities
+{
+    Logger& Logger::getInstance()
+    {
+        static Logger instance;
+        return instance;
+    }
+
+    void Logger::setLogLevel(LogLevel level)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        logLevel_ = level;
+    }
+
+    void Logger::setOutput(Output output)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        output_ = output;
+    }
+
+    void Logger::log(Logger::LogLevel level, std::string_view message)
+    {
+        if (level < logLevel_) return;
+
+        std::lock_guard<std::mutex> lock(mutex_);
+        std::string logEntry = std::format("{} [{}] {}\n", getCurrentTime(), to_string(level), message);
+
+        if (output_ == Output::Console || output_ == Output::All)
+            std::cout << logEntry;
+        if (output_ == Output::File || output_ == Output::All)
+        {
+            std::ofstream logFile("Logs.txt", std::ios_base::app);
+            logFile << logEntry;
+        }
+    }
+
+    std::future<void> Logger::logAsync(Logger::LogLevel level, std::string_view message)
+    {
+        if (level < logLevel_) return std::async([]{});
+        return std::async(std::launch::async, &Logger::log, this, level, message);
+    }
+
+    void Logger::info(std::string_view message)
+    {
+        log(LogLevel::Info, message);
+    }
+
+    void Logger::warning(std::string_view message)
+    {
+        log(LogLevel::Warning, message);
+    }
+
+    void Logger::error(std::string_view message)
+    {
+        log(LogLevel::Error, message);
+    }
+
+    std::future<void> Logger::infoAsync(std::string_view message)
+    {
+        return logAsync(LogLevel::Info, message);
+    }
+
+    std::future<void> Logger::warningAsync(std::string_view message)
+    {
+        return logAsync(LogLevel::Warning, message);
+    }
+
+    std::future<void> Logger::errorAsync(std::string_view message)
+    {
+        return logAsync(LogLevel::Error, message);
+    }
+
+    std::string Logger::getCurrentTime() const
+    {
+        using std::chrono::_V2::system_clock;
+        using std::chrono::duration_cast;
+        using std::chrono::milliseconds;
+        using millisec = std::chrono::duration<int64_t, std::milli>;
+
+        auto now = system_clock::now();
+        time_t in_time_t = system_clock::to_time_t(now);
+        millisec milliseconds_ = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+
+        std::ostringstream ss;
+        ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %H:%M:%S");
+        ss << '.' << std::setfill('0') << std::setw(3) << milliseconds_.count();
+
+        return ss.str();
+    }
+
+    const char* Logger::to_string(LogLevel level) const
+    {
+        switch (level) {
+            case LogLevel::Info: return "INFO";
+            case LogLevel::Warning: return "WARNING";
+            case LogLevel::Error: return "ERROR";
+            default: return "UNKNOWN";
+        }
+    }
+}
---
+++ SMTPServer.Core/Source/Utilities/Logger.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "PCH.h"
+#include "Interfaces/ILogger.h"
+
+namespace SMTPServer::Core::Utilities { class Logger; }
+
+class SMTPServer::Core::Utilities::Logger : public SMTPServer::Core::Interfaces::ILogger
+{
+public:
+    static Logger& getInstance();
+    void setLogLevel(LogLevel level) override;
+    void setOutput(Output output) override;
+
+    // Sync APIs
+    void info(std::string_view message) override;
+    void warning(std::string_view message) override;
+    void error(std::string_view message) override;
+
+    // Async APIs
+    std::future<void> infoAsync(std::string_view message) override;
+    std::future<void> warningAsync(std::string_view message) override;
+    std::future<void> errorAsync(std::string_view message) override;
+
+    ~Logger() override = default;
+
+private:
+    LogLevel logLevel_;
+    Output output_;
+    mutable std::mutex mutex_;
+
+    Logger(const Logger&) = delete;
+    Logger() : logLevel_(LogLevel::Info), output_(Output::Console) {}
+
+    Logger& operator=(const Logger&) = delete;
+
+    std::string getCurrentTime() const;
+    const char* to_string(LogLevel level) const;
+    void log(LogLevel level, std::string_view message);
+    std::future<void> logAsync(LogLevel level, std::string_view message);
+};
