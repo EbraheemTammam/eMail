3f9aed7d437a74867e11e845a76fd56f6363d157
Ebraheem <ebraheem20012020@gmail.com>
2024-11-02 06:37:19 +0200

    implement custom thread-safe queue

---
+++ SMTPServer.Core/Source/Utilities/Queue.cpp
@@ -0,0 +1,60 @@
+#include "Queue.h"
+
+namespace SMTPServer::Core::Utilities
+{
+    template<typename DataType>
+    const DataType& Queue<DataType>::Front() const
+    {
+        std::scoped_lock lock(mutex_);
+        if (front_ == back_) throw std::runtime_error("Queue is empty");
+        return &queue_[front_];
+    }
+
+    template<typename DataType>
+    void Queue<DataType>::Push(const DataType& data)
+    {
+        std::scoped_lock lock(mutex_);
+        if (back_ == Q_SIZE)
+        {
+            if(front_ == 0)
+                throw std::runtime_error("Queue is full");
+            else
+                AlignFront_();
+        }
+        queue_[back_] = data;
+        ++back_;
+    }
+
+    template<typename DataType>
+    DataType Queue<DataType>::Pop()
+    {
+        std::scoped_lock lock(mutex_);
+        if (front_ == back_) throw std::runtime_error("Queue is empty");
+        return queue_[front_++];
+    }
+
+    template<typename DataType>
+    bool Queue<DataType>::IsEmpty() const
+    {
+        std::scoped_lock lock(mutex_);
+        return front_ == back_;
+    }
+
+    template<typename DataType>
+    std::size_t Queue<DataType>::Size() const
+    {
+        std::scoped_lock lock(mutex_);
+        return back_ - front_;
+    }
+
+    template<typename DataType>
+    void Queue<DataType>::AlignFront_()
+    {
+        uint32_t segment = back_ - front_;
+        for(int i = 0; i < segment; ++i)
+            queue_[i] = queue_[i + front_];
+
+        front_ = 0;
+        back_ = segment;
+    }
+}
---
+++ SMTPServer.Core/Source/Utilities/Queue.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "PCH.h"
+
+namespace SMTPServer::Core::Utilities { template<typename DataType> class Queue; }
+
+template<typename DataType>
+class SMTPServer::Core::Utilities::Queue
+{
+private:
+    std::array<DataType, Q_SIZE> queue_;
+    std::mutex mutex_;
+    std::size_t front_ = 0;
+    std::size_t back_ = 0;
+
+    void AlignFront_();
+
+public:
+    Queue() = default;
+    Queue(const Queue&) = delete;
+    Queue(Queue&) = delete;
+
+    Queue& operator=(const Queue&) = delete;
+    Queue& operator=(Queue&) = delete;
+
+    const DataType& Front() const;
+    void Push(const DataType& data);
+    DataType Pop();
+
+    bool IsEmpty() const;
+    std::size_t Size() const;
+};
