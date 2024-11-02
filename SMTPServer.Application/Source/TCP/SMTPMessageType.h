18888024c0baca31ef310e3ec617a920a4f17966
Ebraheem <ebraheem20012020@gmail.com>
2024-11-02 06:43:02 +0200

    custom message type

---
+++ SMTPServer.Application/Source/TCP/SMTPMessageType.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <PCH.h>
+
+namespace SMTPServer::Application::TCP { enum class SMTPMessageType; }
+
+enum class SMTPServer::Application::TCP::SMTPMessageType : uint32_t
+{
+
+};
